05a94cc2f5d6515a28e8a51caa2771c4befd8664
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2023-09-23 00:46:23 -0400

    Update Location.java

--- Project1/DemoOOP/Location.java
+++ Project1/DemoOOP/Location.java
@@ -1,1 +1,26 @@
+public enum Location{
 
+    HLL114("Hill Center", "Busch"),
+    ARC103("Allison Road Classroom", "Busch"),
+    BE_AUD("Beck Hall", "Livingston"),
+    TIL232("Tillett Hall", "Livingston"),
+    AB2225("Academic Building", "College Avenue"),
+    MU302("Murray Hall", "College Avenue");
+
+    private final String BUILDINGNAME;  //do these have to be static??
+    private final String CAMPUS;
+
+    Location(String building, String campus){
+        this.BUILDINGNAME=building;
+        this.CAMPUS=campus;
+    }
+
+    public String getBuildingName(){
+        return this.BUILDINGNAME;
+    }
+    public String getCampus(){
+        return this.CAMPUS;
+    }
+
+
+}
