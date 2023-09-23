5c0174f343bc8005f55e8f531e2b6bfbe6709841
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2023-09-23 00:47:38 -0400

    Add files via upload

---
+++ Project1/DemoOOP/Department.java
@@ -0,0 +1,29 @@
+public enum Department {
+
+CS("Computer Science"), 
+EE("Electrical Engineering"),
+ITI("Information Technology and Informatics"), 
+MATH("Mathematics"), 
+BAIT("Business Analytics and Information Technology");
+
+private final String DEPARTMENTNAME;
+
+Department(String department){
+     this.DEPARTMENTNAME=department;
+}
+
+public String getDepartmentName(){
+     return this.DEPARTMENTNAME;
+}
+
+    /**
+     * Locations:
+     * HLL114, Hill Center, Busch
+ARC103, Allison Road Classroom, Busch
+BE_AUD, Beck Hall, Livingston
+TIL232, Tillett Hall, Livingston
+AB2225, Academic Building, College Avenue
+MU302, Murray Hall, College Avenue
+     */
+    
+}
