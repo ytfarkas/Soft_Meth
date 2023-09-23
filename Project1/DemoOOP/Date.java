a48f8b6535463b222d317763fac8ff1b5fd15911
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2023-09-23 00:45:09 -0400

    Update Date.java

--- Project1/DemoOOP/Date.java
+++ Project1/DemoOOP/Date.java
@@ -1,1 +1,17 @@
+public class Date implements Comparable<Date> { //add comparable method
+    private int year;
+    private int month;
+    private int day;
+    public boolean isValid() //check if the date is a valid calendar date
 
+
+    public int getYear(){
+        return year;
+    }
+    public int getMonth(){
+        return month;
+    }
+    public int getDay(){
+        return day;
+    }
+    }
