f11f3b9325a7783b3288e28d1f24e18b807ccc31
ytfarkas <ytfarkas@gmail.com>
2023-09-22 21:00:55 -0400

    added code to timeslot

--- Project1/DemoOOP/Timeslot.java
+++ Project1/DemoOOP/Timeslot.java
@@ -1,1 +1,18 @@
+class Timeslot{
+    //private classes
+    private String timeslot;
 
+    public Timeslot(String timeslot){
+        this.timeslot = timeslot;
+    }
+
+    public getTimeslot(){
+        return timeslot;
+    }
+
+    public void setTimeslot(String timeslot){
+        if (timeslot == "morning" !! timeslot == "afternoon" !! timeslot == "evening"){
+            this.timeslot = timeslot;
+        }
+    }
+}
