10cb21eb24afed55d1fb3c7c2ef34c97cc302e5e
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2023-09-23 00:43:45 -0400

    Update Contact.java

--- Project1/DemoOOP/Contact.java
+++ Project1/DemoOOP/Contact.java
@@ -1,1 +1,22 @@
+public class Contact {
+    private Department department;
+    private String email;
+    public boolean isValid(){  //complete this 
+
+    }
+
+    Contact(Department department, String email){
+        this.department=department;
+        this.email=email;
+    }
+
+    public Department getDepartment(){
+        return this.department;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+
+
+    }
 
