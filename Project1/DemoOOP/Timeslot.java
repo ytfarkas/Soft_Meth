5fa1d6344620e668813b517bd6811b14baddd789
David Rahabi <davidrahabi@gmail.com>
2023-09-23 15:37:32 -0400

    pongininson i tested timeslot and changed some bugs :w
    :w

--- Project1/DemoOOP/Timeslot.java
+++ Project1/DemoOOP/Timeslot.java
@@ -66,6 +66,7 @@ public enum Timeslot{
 
 
              hours+=hourToAdd;
+             
              minutes+=minutesToAdd; //new hr and min values
              if (minutes>=60){
                 
@@ -86,24 +87,80 @@ public enum Timeslot{
         
 
              if(hours>=12){
-                if (hours>12){
-                hours-=12; //you only want to do this if hours is greater than, not equal to bc then itd be zero
+
+               /*  
+                THIS IS PREVIOUS CODE FROM BEFORE THAT IS REPLACED BELOW
+               if (hours>24){
+                    hours-=24;  //this is in the case that the duration inputted is like 23hours
+                                //ex: 2:00+23 hours= 25 
+                                //25-24=1 (23 hours after 2PM would be 1PM)
                 }
+                else if (hours>12){
+
+                hours-=12; //you only want to do this if hours is greater than, not equal to bc then itd be zero
+                }*/
+
+
+
                 /**
-                 * now switch am/pm value only if it went from a lower number than 12, 
-                 * ex: in this if statement the case of it going from 12-1 is possible, and you dont want to 
-                 * change am/pm value then
-                 * 
+                 * now switch am/pm value accordingly
+                 * check explanation below for why you use %2==1 to change ampm value 
                  */
                 
-                if(oldHours<12){
-                    if(amOrpm.equals("pm")){
-                      amOrpm="am";
-                 }
-                    else{
-                      amOrpm="pm";
+                
+                    
+                    if((hours/12)%2==1){     
+                        if(amOrpm.equals("pm")){
+                            amOrpm="am";
+                       }
+                          else{
+                            amOrpm="pm";
+                          }
                     }
+
+                      /*
+             * this part you really have to draw out to understand why it works for changing the am/pm value properly.
+             * above in the code i commented out, i just tried to hardcode if the hours was either greater than 12 or greater then 24, 
+             * but what if the user inputs 48 hours or even 100 hours??
+             * 
+             * in order to calculate when the change the am/pm value, you need to understand when the am/pm value gets changed and when it doesn't get changed
+             * whenever the time increased is<12 hours, the am/pm value wont change
+             * whenever the time increased is 12<x<24 (x for time increased), ampm value will change ex: 1:00pm + 13 hours= 2:00am
+             * whenever the time increased is 24<x<36, the ampm value will not change ex: 1:00pm + 25 hours = 2:00pm
+             * and this pattern continues
+             * 
+             * what can you do with this?
+             * 
+             * lets say for 12<x<24, all those numbers divided by 12 are 1 when using INTEGER DIVISION, ex: 12/13=1 12/23=1
+             * and for 24<x<36, all those numbers divided by 12 are 2 using integer division, ex: 12/25=2 12/35=2
+             * 
+             * so in order to caputer this pattern as it goes into higher numbers, you need to use the %2 to check whether to change the ampm value
+             * notice above how if you do (12/13)%2 it will equal 1, and the ampm value will change
+             *  (12/25)%2=0, so the ampm value wont change
+             *  this pattern will continue for higher numbers ex: 2:00pm + 47hours = 1:00pm --> 47/12=3 --> 3%2==1 so you change the ampm value
+             *                                                    
+             */
+
+           // }
+
+            
+            hours=hours-((hours/12)*12); //this calculates what the time should be e
+                                /*
+                                 * ex: say its 2:00pm and you add 49 hours, that would be 3:00pm
+                                 * hours=2, hoursToAdd=49, hours+hourstoadd=51
+                                 * 51/12=4
+                                 * 12*4=48
+                                 * 
+                                 * 51-((51/12)*12) = 51-48=3 
+                                 */
+
+            if (hours==0){
+                hours=12;
+                //the above equation produces 0 if the hours gets added up to an exact multiple of 12 like 24,48, or 60
+                //this is an edge case where you can just convert from 0 to 12
             }
+
+          
         }
 
         if (minutes<10){
