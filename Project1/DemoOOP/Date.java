2bc6269bc720d208e196f0666ceb3385311c9cc3
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2023-09-27 22:29:31 -0400

    Update Date.java
    added equals() and compareTo() - need to check if compareTo is done correctly

--- Project1/DemoOOP/Date.java
+++ Project1/DemoOOP/Date.java
@@ -14,4 +14,33 @@ public class Date implements Comparable<Date> { //add comparable method
     public int getDay(){
         return day;
     }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Date){
+            Date date = (Date) obj;
+
+            return date.year==this.year && date.month==this.month && date.day==this.day;
+
+        }
+    }
+
+
+    @Override
+    public int compareTo(Date date){
+        if(this.year>date.year) return 1;       //compare years first
+        else if(date.year>this.year) return -1;
+        else{//if they are equal
+            if(this.month>date.month) return 1; //compare months
+            else if(date.month>this.month) return -1;
+            else{ //if months are equal
+                if(this.day>date.day) return 1; //compare days
+                else if(date.day>this.day) return -1;
+                else return 0; //if days are equal
+            }
+        }
+        /*
+         * NEED TO CHECK IS THIS IS OK TO DO/ THIS IS HOW WE ARE SUPPOSED TO DO THIS
+         */
+    }
     }
