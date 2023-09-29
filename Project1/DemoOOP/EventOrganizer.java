524b12a71623bf8849507253d2561d862bf65103
ytfarkas <ytfarkas@gmail.com>
2023-09-29 03:40:24 -0400

    babbaboy

--- Project1/DemoOOP/EventOrganizer.java
+++ Project1/DemoOOP/EventOrganizer.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+public class EventOrganizer{
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Event Organizer running....");
+        String input = scanner.nextLine();
+
+        while (true){
+            if (input.equals("Q")){
+            System.out.println ("Event Organizer terminated");
+                break;
+            }
+            switch(input){
+                case "A":
+                Date date = new Date(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                Timeslot timeslot = new Timeslot(scanner.nextLine());
+                Location location = new Location(scanner.nextLine());
+                Department department = new Department( scanner.nextLine());
+                Contact contact = new Contact(scanner.nextLine(), scanner.nextLine());
+
+                case "P":
+                EventCalendar.print();
+                case "PE":
+                case "PC":
+                case "PD":
+                
+            }
+
+            
+        }
+
+    }
+
+}
